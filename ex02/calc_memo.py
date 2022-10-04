8703bf7edc05c974a9602a76cf2fffd9c0d7db4f
Kazuki Akiyama <c0b2117168@edu.teu.ac.jp>
2022-10-04 18:32:34 +0900

    #5 Equal Button

---
+++ ex02/calc_memo.py
@@ -0,0 +1,29 @@
+####print("hello world")
+#import tkinter as tk
+#import tkinter.messagebox as tkm 
+#root = tk.Tk()
+#root.title("お試しか")
+#root.geometry("500x200")
+#label = tk.Label(root,
+ #               text="ラベルを書いてみた件",
+  #              font= ("Ricty Diminished",20))
+#label.pack()
+
+
+#entry =tk.Entry(width=30)
+#entry.insert(tk.END,"fugapiyo")
+#entry.pack()
+
+#def button_click(event):
+    #tkm.showwarning("警告","ボタン押したらあかん言うたやろ")
+    #btn =event.widget
+    #txt = btn["text"]
+    #tkm.showinfo(txt,f"[{txt}]ボタンが押されました")  
+#button =tk.Button(root,text="押すな",command=button_click)
+#button.pack()
+#button =tk.Button(root,text="押すな",)
+#button.bind("<1>",button_click) #,command=button_click)
+#button.pack()
+#root.mainloop()
+
+
