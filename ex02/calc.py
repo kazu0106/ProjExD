565143dbfa259a639157432b4cb2076f14551aa3
Kazuki Akiyama <c0b2117168@edu.teu.ac.jp>
2022-10-04 17:17:21 +0900

    電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,39 +1,53 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def click_number(event):
+def click_number(event):#数字の入力に対する対応
     btn = event.widget
     num = btn["text"]
     entry.insert(tk.END, num) 
+    b = event.widget.flash()#点滅をさせる部分 
 
 
-def click_equal(event):
+def click_equal(event):#=に関する入力に対する対応
     eqn = entry.get()
     res = eval(eqn)
     entry.delete(0, tk.END)
-    entry.insert(tk.END, res) 
+    entry.insert(tk.END, res)
+    b = event.widget.flash()#点滅をさせる部分
 
 
-root = tk.Tk() # 練習1
+root = tk.Tk() 
 root.geometry("300x500")
 
-entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練
 entry.grid(row=0, column=0, columnspan=3)
 
-r, c = 1, 0 # r: 行を表す変数／c：列を表す変数
-numbers = list(range(9, -1, -1)) # 数字だけのリスト
-operators = ["+"] # 演算子だけのリスト
-for i, num in enumerate(numbers+operators, 1):
+r, c = 1, 0 # 
+numbers = list(range(9, -1, -1)) # 
+operators = ["+"] # ＋だけ用意
+
+
+
+for i, num in enumerate(numbers+operators, 1):#電卓を造る
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click_number)
-    btn.grid(row=r, column=c)
+    
+    b = tk.Button(text=f"{num}", bg='light blue',font=("",30),width=4,height=2)#点滅を実装
+    b.bind('<1>' ,click_number) 
+    
+    btn.grid(row=r, column=c)   #縦横を仕切る
+    b.grid(row=r, column=c)
     c += 1
-    if i%3 == 0:
+    
+    if i%3 == 0: #縦横　3列　4列の判別　
         r += 1
         c = 0
 
-btn = tk.Button(root, text=f"=", font=("", 30), width=4, height=2)
+
+btn = tk.Button(root, text=f"=", font=("", 30),bg='red', width=4, height=2)
+
 btn.bind("<1>", click_equal)
-btn.grid(row=r, column=c)
+
+btn.grid(row=r, column=c) #=を実装
 
 root.mainloop()
