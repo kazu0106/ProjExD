e8b2cbaf1c26ef2261fcc21d1cda830866b57a4a
Kazuki Akiyama <c0b2117168@edu.teu.ac.jp>
2022-10-18 17:12:10 +0900

    迷路の詳細

--- ex03/maze_maker.py
+++ ex03/maze_maker.py
@@ -7,15 +7,19 @@ def make_maze(yoko, tate):
     maze_lst = []
     for y in range(tate):
         maze_lst.append([0]*yoko)
+    
     for x in range(yoko):
         maze_lst[0][x] = 1
         maze_lst[tate-1][x] = 1
+    
     for y in range(1, tate-1):
         maze_lst[y][0] = 1
         maze_lst[y][yoko-1] = 1
+    
     for y in range(2, tate-2, 2):
         for x in range(2, yoko-2, 2):
             maze_lst[y][x] = 1
+    
     for y in range(2, tate-2, 2):
         for x in range(2, yoko-2, 2):
             if x > 2: rnd = random.randint(0, 2)
@@ -26,6 +30,7 @@ def make_maze(yoko, tate):
 
 def show_maze(canvas, maze_lst):
     color = ["white", "gray"]
+    
     for y in range(len(maze_lst)):
         for x in range(len(maze_lst[y])):
             canvas.create_rectangle(x*100, y*100, x*100+100, y*100+100, 
