05f4b740303a675d4aaeaf818d2493ee5d5c081f
Kazuki Akiyama <c0b2117168@edu.teu.ac.jp>
2022-10-18 15:14:42 +0900

    迷えるこうかとん

---
+++ ex03/maze_koukaton.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+import maze_maker as mm
+
+def key_down(event):
+    global key
+    key = event.keysym
+
+
+
+def key_up(event):
+    global key
+    key = ""
+
+
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx += 20
+    canv.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん") 
+
+   
+    canv = tk.Canvas(root, width=1500, height=900, bg="black")
+    canv.pack()
+
+    tori = tk.PhotoImage(file="ex03/fig/fig/5.png") 
+    cx, cy = 300, 400
+    canv.create_image(cx, cy, image=tori, tag="tori")
+
+    
+    key = "" 
+
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)    
+
+    main_proc()
+    maze_list= mm.make_maze(15,9)
+    #print(maze_list)debagg
+    mm.show_maze(canv,maze_list)
+    root.mainloop()
