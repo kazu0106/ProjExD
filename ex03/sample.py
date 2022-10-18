18ca2b2613eac7fbb74b2cc7a61da4b77e753514
Kazuki Akiyama <c0b2117168@edu.teu.ac.jp>
2022-10-18 17:25:24 +0900

    迷路の説明2

---
+++ ex03/sample.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import tkinter.messagebox as tkm 
+
+def count_up():
+    global tmr,jid
+    tmr=tmr+1
+    label["text"]=tmr
+    jid= root.after(1000,count_up)
+
+def key_down(event):
+    global jid 
+    if jid != None:
+        root.after_cancel(jid)
+        jid =None
+        return 
+    #key = event.keysym
+   #tkm.showinfo("キー押した",f"{key}キーが押されました")
+    jid=root.after(1000,count_up)
+
+
+if __name__ =="__main__":
+    root=tk.Tk()
+    label =tk.Label(root,font=("",80)) 
+    label.pack()
+
+    tmr = 0
+    jid=None
+    #root.after(1000,count_up)
+    root.bind("<KeyPres>",key_down)
+    root.mainloop()
